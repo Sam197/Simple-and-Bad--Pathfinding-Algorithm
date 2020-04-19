fe3eb375f08cd8e4b325b9aeee7cbc134f3f29d1
samuel jones <sojonesey@gmail.com>
2020-04-19 14:55:54 +0100

    added comments

--- yeswithvisuels.py
+++ yeswithvisuels.py
@@ -3,8 +3,12 @@ import pygame
 from pygame import *
 import time
 
-SHOW_AT_ALL = False
-SHOW_STEP = False
+SHOW_AT_ALL = True    #For speed, doesn't have any visuals
+SHOW_STEP = True      #Shows each iteration of the loops
+STEP_BY_STEP = False  #Iterates over in the loop on mouse click
+
+if STEP_BY_STEP:
+    SHOW_STEP = True  #These staments are here to make sure things won't crash if I'm an idiot
 
 if SHOW_STEP:
     SHOW_AT_ALL = True
@@ -12,22 +16,29 @@ if SHOW_STEP:
 if not SHOW_AT_ALL:
     SHOW_STEP = False
 
+if not SHOW_STEP:
+    STEP_BY_STEP = False
+
+
 if SHOW_AT_ALL:
     pygame.init()
 
 #Max X = 93 Y = 47   #Can get to around 1750 obsticles
 
-GRIDX = 200
-GRIDY = 200
-obsticals = 1000
-#obsticals = (GRIDX*GRIDY)*0.4
+GRIDX = 93
+GRIDY = 47
+obsticals = 1600
+#obsticals = (GRIDX*GRIDY)*0.4     #Genral max amount of obsitcals in the grid
 global attempts
 attempts = 0
 TILE_SIZE = 20
 if SHOW_AT_ALL:
     FONT = pygame.font.SysFont('comicsans', 20)
 
-print("Oh dear")
+global startstart
+startstart = time.time()
+
+print("Oh dear")            #First line of code written
 
 def make_grid():
     grid = []
@@ -50,7 +61,7 @@ def make_grid():
     grid.append(placeholder)
     return grid
 
-def find_coords(grid, num):
+def find_coords(grid, num):    #Finds the coordiates of any instance of a given int or char and returns a list of those coords
     Ycoord = 0
     coords = []
     for row in grid:
@@ -62,7 +73,7 @@ def find_coords(grid, num):
         Ycoord += 1
     return coords
 
-def bubbleSort(testList, otherList):
+def bubbleSort(testList, otherList):    #Bubble sort ftw
     while True:
         through = True
         for x in range(0,len(testList)-1):
@@ -78,23 +89,23 @@ def bubbleSort(testList, otherList):
                 through = False
         #print(testList)
         if through == True:
-            return otherList
+            return otherList          #Vauge variables ftw
 
-def print_grid(grid):
+def print_grid(grid):               #Method that prints the grid to the console, not used anymore 
 
     for row in grid:
         for col in row:
-            print(col, end = " ")
+            print(col, end = " ")    
         print()
 
-def show_to(grid, screen):
+def show_to(grid, screen):          #Displays the grid to pygame window
     screen.fill((255,255,255))
     ycoord = 0
     for row in grid:
         xcoord = 0
         for col in row:
-            if not str(col).isdigit() and col != "#" and col != "E":
-                colour = (255,0,0)
+            if not str(col).isdigit() and col != "#" and col != "E":    #Chnages colour of objects depending on what it is
+                colour = (255,0,0)                                      #Yes this logic could be a lot better
             elif str(col).isdigit() and col != 0:
                 colour = (0,0,255)
             elif col == "#":
@@ -110,10 +121,11 @@ def show_to(grid, screen):
 
 def main():
     
+    global startstart     #I hate the fact I have to delcare these as globals, there already globals
     global attempts
     attempts += 1
     if SHOW_AT_ALL:
-        screen = pygame.display.set_mode(((GRIDX+2)*TILE_SIZE, (GRIDY+2)*TILE_SIZE))
+        screen = pygame.display.set_mode(((GRIDX+2)*TILE_SIZE, (GRIDY+2)*TILE_SIZE))     #Setting the screen size, avoding magic numbers
         #screen = pygame.display.set_mode((440, 440))
         screen.fill((255,255,255))
 
@@ -121,10 +133,10 @@ def main():
     starting_grid = make_grid()
     x = 0
 
-    while x < obsticals:
+    while x < obsticals:                        #Add in obsicals randomly
         gy = random.randrange(0, GRIDY + 1)
         gx = random.randrange(0, GRIDX + 1)
-        if grid[gy][gx] != "#":
+        if grid[gy][gx] != "#":               
             grid[gy][gx] = "#"
             starting_grid[gy][gx] = "#"
             x += 1
@@ -140,6 +152,7 @@ def main():
     # grid[Fy][Fx] = "F"
     # starting_grid[Fy][Fx] = "F"
 
+    #Chosing set start and end points
     curX = 2
     curY = 2
     grid[curY][curX] = "S"
@@ -148,7 +161,7 @@ def main():
     starting_grid[GRIDY-2][GRIDX-2] = "F"
 
     #print_grid(grid)
-    if SHOW_STEP:
+    if SHOW_AT_ALL:
         show_to(grid, screen)
 
     step = 0
@@ -156,52 +169,62 @@ def main():
     coords = [(curY, curX)]
     notFound = True
     start = time.time()
+    moveon = True
 
     while notFound:
-        if SHOW_AT_ALL:
-            pygame.event.pump()
-        step += 1
-        for curY, curX in coords:
-            if grid[curY][curX + 1] == 0:
-                grid[curY][curX + 1] = step
-            if grid[curY][curX - 1] == 0:
-                grid[curY][curX - 1] = step
-            if grid[curY + 1][curX] == 0:
-                grid[curY + 1][curX] = step
-            if grid[curY - 1][curX] == 0:
-                grid[curY - 1][curX] = step
-            if grid[curY][curX + 1] == "F":
-                notFound = False
-            if grid[curY][curX - 1] == "F":
-                notFound = False
-            if grid[curY + 1][curX] == "F":
-                notFound = False
-            if grid[curY - 1][curX] == "F":
-                notFound = False   
 
-        if len(coords) == 0:
-            curSteps += 1
-        if curSteps > 1:
-            if SHOW_AT_ALL:
-                pygame.display.set_caption("Impossible")
-        if curSteps == 10:
-            #screen.blit(text, (((GRIDX+2)*TILE_SIZE)/2, ((GRIDX+2)*TILE_SIZE)/2))
-            del grid
-            del starting_grid
-            main()
-                
+        if STEP_BY_STEP:
+            moveon = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed()[0]:
+                        moveon = True
 
-        #time.sleep(0.1)
-        #print_grid(grid)
-        if SHOW_STEP:
-            show_to(grid, screen)
-        coords = find_coords(grid, step)
         if SHOW_AT_ALL:
-            pygame.display.set_caption(str(step) + " step" + "   " + str(attempts) + " attempts")
-        #print(coords)
-        #print(step)
+            pygame.event.pump()   #This allows pygame to go through it's event magement, stops it going into no respond mode
+
+        if moveon:
+            step += 1
+            for curY, curX in coords:
+                if grid[curY][curX + 1] == 0:
+                    grid[curY][curX + 1] = step
+                if grid[curY][curX - 1] == 0:
+                    grid[curY][curX - 1] = step
+                if grid[curY + 1][curX] == 0:
+                    grid[curY + 1][curX] = step
+                if grid[curY - 1][curX] == 0:
+                    grid[curY - 1][curX] = step
+                if grid[curY][curX + 1] == "F":
+                    notFound = False
+                if grid[curY][curX - 1] == "F":
+                    notFound = False
+                if grid[curY + 1][curX] == "F":
+                    notFound = False
+                if grid[curY - 1][curX] == "F":
+                    notFound = False   
+
+            if len(coords) == 0:    #If no coords returned, it is impossible
+                curSteps += 1
+            if curSteps > 1:
+                if SHOW_AT_ALL:
+                    pygame.display.set_caption("Impossible")
+            if curSteps == 10:       #Resets map
+                del grid
+                del starting_grid
+                main()
+                    
+
+            #time.sleep(0.1)
+            #print_grid(grid)
+            if SHOW_STEP:
+                show_to(grid, screen)
+            coords = find_coords(grid, step)
+            if SHOW_AT_ALL:                     #Long line - ooooooooooooo
+                pygame.display.set_caption(str(step) + " step" + "   " + str(attempts) + " attempts     " + "Cur Time " + str(time.time()-start) + "   " + "Total Time " + str(time.time()-startstart ))
+            #print(coords)
+            #print(step)
     
-    for curY, curX in find_coords(grid, 0):
+    for curY, curX in find_coords(grid, 0):   #E
         grid[curY][curX] = "E"
 
     coords = find_coords(grid, "F")
@@ -209,46 +232,55 @@ def main():
     #print_grid(grid)
     #show_to(grid, screen)
     Not_back = True
+    moveon = True
 
     while Not_back:
         if SHOW_AT_ALL:
             pygame.event.pump()
         numsAround = []
         otherList = []
-        if str(grid[curY][curX + 1]).isdigit():
-            numsAround.append(grid[curY][curX + 1])
-            otherList.append("Right")
-        if str(grid[curY][curX + 1]) == "S":
-            break
-        if str(grid[curY][curX - 1]).isdigit():
-            numsAround.append(grid[curY][curX - 1])
-            otherList.append("Left")
-        if str(grid[curY][curX - 1]) == "S":
-            break
-        if str(grid[curY + 1][curX]).isdigit():            
-            numsAround.append(grid[curY + 1][curX])
-            otherList.append("Down")
-        if str(grid[curY + 1][curX]) == "S":
-            break
-        if str(grid[curY - 1][curX]).isdigit():
-            numsAround.append(grid[curY - 1][curX])
-            otherList.append("Up")
-        if str(grid[curY - 1][curX]) == "S":
-            break
-        directions = bubbleSort(numsAround, otherList)
-        if directions[0] == "Right":
-            curX += 1
-        elif directions[0] == "Left":
-            curX -= 1
-        elif directions[0] == "Down":
-            curY += 1
-        elif directions[0] == "Up":
-            curY -= 1
-        
-        #time.sleep(0.1)
-        grid[curY][curX] = "P"
-        if SHOW_STEP:
-            show_to(grid, screen)
+        if STEP_BY_STEP:
+            moveon = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed()[0]:
+                        moveon = True
+
+        if moveon:    
+            if str(grid[curY][curX + 1]).isdigit():       #.isdigit() checks for number
+                numsAround.append(grid[curY][curX + 1])
+                otherList.append("Right")
+            if str(grid[curY][curX + 1]) == "S":
+                break
+            if str(grid[curY][curX - 1]).isdigit():
+                numsAround.append(grid[curY][curX - 1])
+                otherList.append("Left")
+            if str(grid[curY][curX - 1]) == "S":
+                break
+            if str(grid[curY + 1][curX]).isdigit():            
+                numsAround.append(grid[curY + 1][curX])
+                otherList.append("Down")
+            if str(grid[curY + 1][curX]) == "S":
+                break
+            if str(grid[curY - 1][curX]).isdigit():
+                numsAround.append(grid[curY - 1][curX])
+                otherList.append("Up")
+            if str(grid[curY - 1][curX]) == "S":
+                break
+            directions = bubbleSort(numsAround, otherList)    #Bubble sort ftw
+            if directions[0] == "Right":
+                curX += 1
+            elif directions[0] == "Left":
+                curX -= 1
+            elif directions[0] == "Down":
+                curY += 1
+            elif directions[0] == "Up":
+                curY -= 1
+            
+            #time.sleep(0.1)
+            grid[curY][curX] = "P"
+            if SHOW_STEP:
+                show_to(grid, screen)
 
     #print_grid(grid)
     if SHOW_STEP:
@@ -274,9 +306,12 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     a = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed()[2]:      #Right click to reset map
+                        del grid
+                        del starting_grid
+                        attempts = 0
+                        startstart = time.time()
+                        main()
 
-    
-
-
-
-main()
+main()   #I always forget this line
